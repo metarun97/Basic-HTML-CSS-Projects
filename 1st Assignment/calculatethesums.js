5110ff7bdce735511f544911d3f568f965dc59c0
TARUN UPADHYAY <116960857+metarun97@users.noreply.github.com>
2022-12-03 15:59:47 +0530

    Add files via upload

---
+++ 1st Assignment/calculatethesums.js
@@ -0,0 +1,11 @@
+// Exercise 3: Calculate the sum of all numbers from 1 to a given number
+// Write a program to accept a number from a user and calculate the sum of all numbers from 1 to a given number
+// For example, if the user entered 10 the output should be 55...? 
+
+let value = 10
+let sum = 55
+while(value<10){
+    sum = sum + value
+    value++
+}
+console.log(sum)
---
+++ 1st Assignment/naturalnumbers.js
@@ -0,0 +1,7 @@
+// Exercise 1: Print First 10 natural numbers using while loop...? 
+
+let i = 1
+while(i<=10){
+    console.log(i)
+    i++
+}
